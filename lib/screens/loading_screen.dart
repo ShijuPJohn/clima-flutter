adef5ed8bc7b239860dd4f45a2ca2755af0ae2a4
ShijuPJohn <shijupjohn4u@gmail.com>
2020-08-11 23:26:19 +0530

    add http get and display response data body in console

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -6,16 +7,28 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  final Location location = Location();
+
+  void getLocation() async {
+    await location.getCurrentLocation();
+    print('latitude : ${location.latitude}');
+    print('longitude : ${location.longitude}');
+    await location.getData();
+    print('data : ${location.response.body}');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+        child: Text('Hello'),
       ),
     );
   }
